0e867886e475b947288d1b00009be6b9a963a44c
Lay Kuan Loh <lkloh2410@gmail.com>
2015-01-06 18:43:31 -0500

    malia younger than mom

--- parse_test.cpp
+++ parse_test.cpp
@@ -364,6 +364,83 @@ void quantifier__function_child_younger_than_mother() {
 }
 
 
+/*
+ * Function
+ * --------
+ * mother(x): returns the mother of x
+ *
+ * Predicate
+ * ---------
+ * YOUNGER(x,y): x is younger than y
+ *
+ * ASSERT forall x, YOUNGER(x, mother(x))
+ * (everyone is younger than his/her mother)
+ * 
+ * QUERY YOUNGER("MaliaObama", mother("MaliaObama"))? //should be true
+ */
+void nested_function_check() {
+    ExprManager em;
+    SmtEngine smt(&em);
+
+    std::cout << "\nNested Function Check" << std::endl;
+
+    /* ---------------------------- RAPIDNET ------------------------------ */
+    //bound vars
+    Variable* child = new Variable(Variable::STRING, true); 
+
+    //mother schema
+    vector<Variable::TypeCode> mother_domain_types;
+    mother_domain_types.push_back(Variable::STRING);
+    FunctionSchema* mother_schema = new FunctionSchema("mother", mother_domain_types, Variable::STRING);
+
+    //mother UserFunction
+    vector<Term*> mother_args;
+    mother_args.push_back(child);
+    UserFunction* mother_function = new UserFunction(mother_schema, mother_args);
+
+    //YOUNGER schema
+    vector<Variable::TypeCode> younger_domain_types;
+    younger_domain_types.push_back(Variable::STRING);
+    younger_domain_types.push_back(Variable::STRING);
+    PredicateSchema* YOUNGER_schema = new PredicateSchema("YOUNGER", younger_domain_types);
+
+    //Younger terms
+    vector<Term*> YOUNGER_args;
+    YOUNGER_args.push_back(child);
+    YOUNGER_args.push_back(mother_function);
+    PredicateInstance* YOUNGER_x_motherx = new PredicateInstance(YOUNGER_schema, YOUNGER_args);
+
+    // forall x, YOUNGER(x, mother(x))
+    vector<Variable*> quantifier_bound_vars;
+    quantifier_bound_vars.push_back(child);
+    Quantifier* forall_x__YOUNGER_x_motherx = new Quantifier(Quantifier::FORALL, quantifier_bound_vars, YOUNGER_x_motherx);
+
+    /* ---------------------------- RAPIDNET ------------------------------ */
+
+    StringVal* MaliaObama = new StringVal("MaliaObama");
+
+    // mother("MaliaObama")
+    vector<Term*> malia_schema_args;
+    malia_schema_args.push_back(MaliaObama);
+    UserFunction* mother_malia = new UserFunction(mother_schema, malia_schema_args);
+
+    vector<Term*> YOUNGER_malia_args;
+    YOUNGER_malia_args.push_back(MaliaObama);
+    YOUNGER_malia_args.push_back(mother_malia);
+    PredicateInstance* malia_younger_than_her_mother = new PredicateInstance(YOUNGER_schema, YOUNGER_malia_args);
+
+    /* ---------------------------- CVC4 ------------------------------ */
+
+    Expr forall_x__YOUNGER_x_motherx_cvc4 = parseFormula(&em, forall_x__YOUNGER_x_motherx);
+    Expr malia_younger_than_her_mother_cvc4 = parseFormula(&em, malia_younger_than_her_mother);
+
+    smt.assertFormula(forall_x__YOUNGER_x_motherx_cvc4);
+    std::cout << "Since "<< forall_x__YOUNGER_x_motherx_cvc4 << " therefore " << malia_younger_than_her_mother_cvc4 << " is: " << smt.query(malia_younger_than_her_mother_cvc4) << std::endl;
+
+    clearAllVariables();
+}
+
+
 int main() {
     testIntegersArithmetic();
     testVariables();
@@ -374,6 +451,7 @@ int main() {
     arithmetic__4_plus_3__minus__2_plus_1__equals__4();
     quantifier__predicate__ancestor();
     quantifier__function_child_younger_than_mother();
+    nested_function_check();
     return 0;
 }
 
